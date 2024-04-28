e45e99c938756639ab1c8d68d4776cd1be40caa0
AgroSmT <agrosmrttech@gmail.com>
2024-04-27 18:49:10 -0600

    first commit

--- AgroWeb/src/screens/loginform.jsx
+++ AgroWeb/src/screens/loginform.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { loginUser } from '../servicios/authService';
-import { useNavigate } from 'react-router-dom'; // Importa useNavigate
-import '../css/login.css';
-import ops from '../componentes/sweet.js'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { loginUser } from "../servicios/authService";
+import { useNavigate } from "react-router-dom"; // Importa useNavigate
+import "../css/login.css";
+import ops from "../componentes/sweet.js";
 
 const LoginForm = () => {
   const { handleSubmit, register } = useForm();
@@ -12,33 +12,48 @@ const LoginForm = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
 
-
-  
   const onSubmitlogin = async (data) => {
-    try{
-
-      // console.log(data);
+    try {
+      console.log(data);
       const res = await loginUser(data);
-      // console.log(res);
+      console.log(res);
       if (res) {
-        // console.log("Usuario logeado");
+        console.log("Usuario logeado");
         setUser(res.data);
         setIsAuthenticated(true);
         setUsername(data.usuario);
-        navigate('/home-screen'); // Redirige al usuario a la pantalla home-screen
+        navigate("/home-screen"); // Redirige al usuario a la pantalla home-screen
       }
-    }catch(error){
-      ops()
+    } catch (error) {
+      ops();
     }
   };
 
   return (
     <div className="login">
       <form>
-        <label htmlFor="chk" aria-hidden="true">Iniciar Sesión</label>
-        <input type="text" name="email" placeholder="Correo electrónico" {...register("email", { required: true })} />
-        <input type="password" name="password" placeholder="Contraseña" {...register("password", { required: true })} />
-        <button type="button" className='button3' onClick={handleSubmit(onSubmitlogin)}>Iniciar sesión</button>
+        <label htmlFor="chk" aria-hidden="true">
+          Iniciar Sesión
+        </label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Correo electrónico"
+          {...register("email", { required: true })}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          {...register("password", { required: true })}
+        />
+        <button
+          type="button"
+          className="button3"
+          onClick={handleSubmit(onSubmitlogin)}
+        >
+          Iniciar sesión
+        </button>
       </form>
     </div>
   );
